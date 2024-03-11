62c7a1ca694ed89412f55745d0bdd6c57b213ad5
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-03-11 14:38:43 -0600

    solution

---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+function Links({linkedin,github}){
+    // console.log(git)
+    return(
+        <>
+            <h3>Links</h3>
+            <a href={linkedin}>{linkedin}</a>
+            <a href={github}>{github}</a>
+        </>
+    )
+}
+export default Links
